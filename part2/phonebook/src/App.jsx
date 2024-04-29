117705384c3df8fb5639397c60486775e6e73db1
FabMira <fmirabella08@gmail.com>
2024-04-29 17:56:14 -0400

    exercise 2.6 step 1 final

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,44 @@
 import { useState } from "react"
+import Persons from "./components/persons"
 
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', id: 0}
   ])
   const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personsObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personsObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Persons key={persons.id} persons={persons} />
     </div>
+    
   )
 }
 
---
+++ part2/phonebook/src/components/persons.jsx
@@ -0,0 +1,11 @@
+const Persons = ({persons}) => {
+    return (
+      <>
+        {persons.map((person) => (
+          <p key={person.id}>{person.name}</p>
+        ))}
+      </>
+    )
+  }
+
+export default Persons
