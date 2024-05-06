5231550bda73dd29392d0209f8097b991e25eddf
FabMira <fmirabella08@gmail.com>
2024-05-06 16:10:59 -0400

    exercise 2.11 step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import Persons from "./components/persons"
 import PersonForm from "./components/PersonForm"
 import Filter from "./components/Filter"
